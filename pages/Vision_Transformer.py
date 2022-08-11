f2898fe0e5c6d16cb197e982aacc0b1ba011b57c
Enrico Shippole <enricoship@gmail.com>
2022-08-10 22:02:52 -0400

    Add gif

---
+++ pages/Vision_Transformer.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title('Vision Transformer')
+
+st.image(
+    "https://media.giphy.com/media/lr9Xd6IkR8F0mAv0bL/giphy.gif",
+    caption="Animated Vision Transformer",
+    #width=400, # The actual size of most gifs on GIPHY are really small, and using the column-width parameter would make it weirdly big. So I would suggest adjusting the width manually!
+)
+
