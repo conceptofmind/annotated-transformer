1495dc89af570616139aa44c352ddb68ae070623
Enrico Shippole <enricoship@gmail.com>
2022-08-10 23:15:14 -0400

    Add introduction

--- pages/Vision_Transformer.py
+++ pages/Vision_Transformer.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+language='python'
+
 st.title('Vision Transformer')
 
 st.image(
@@ -8,3 +10,54 @@ st.image(
     #width=400, # The actual size of most gifs on GIPHY are really small, and using the column-width parameter would make it weirdly big. So I would suggest adjusting the width manually!
 )
 
+st.header('Table of contents')
+
+st.header('Introduction')
+
+st.markdown('''"
+While the Transformer architecture has become the de-facto standard for natural language processing tasks, its applications to computer vision remain limited. In vision, attention is either applied in conjunction with convolutional networks, or used to replace certain components of convolutional networks while keeping their overall structure in place. We show that this reliance on CNNs is not necessary and a pure transformer applied directly to sequences of image patches can perform very well on image classification tasks. When pre-trained on large amounts of data and transferred to multiple mid-sized or small image recognition benchmarks (ImageNet, CIFAR-100, VTAB, etc.), Vision Transformer (ViT) attains excellent results compared to state-of-the-art convolutional networks while requiring substantially fewer computational resources to train.
+" - Alexey Dosovitskiy, Lucas Beyer, Alexander Kolesnikov, Dirk Weissenborn, Xiaohua Zhai, Thomas Unterthiner, Mostafa Dehghani, Matthias Minderer, Georg Heigold, Sylvain Gelly, Jakob Uszkoreit, Neil Houlsby''')
+
+st.header('Imports')
+
+imports_tab_1, imports_tab_2 = st.tabs(["Haiku", "PyTorch"])
+
+with imports_tab_1:
+    haiku_imports = '''
+from functools import partial
+
+import haiku as hk
+from haiku import PRNGSequence
+
+import jax
+from jax import random, nn
+import jax.numpy as jnp
+
+from einops import rearrange, repeat
+    '''
+    st.code(haiku_imports, language)
+
+with imports_tab_2:
+    pytorch_imports = '''
+import torch
+from torch import nn
+
+from einops import rearrange, repeat
+from einops.layers.torch import Rearrange
+    '''
+    st.code(pytorch_imports, language)
+
+
+
+
+
+
+st.header('Model Architecture')
+
+
+
+st.header('Training')
+
+st.header('References')
+
+st.header('Citations')
