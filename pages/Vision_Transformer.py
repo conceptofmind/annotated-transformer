a51cef154e6c0812c555f39c14883691fd28332d
Enrico Shippole <enricoship@gmail.com>
2022-08-11 22:29:48 -0400

    Add code

--- pages/Vision_Transformer.py
+++ pages/Vision_Transformer.py
@@ -21,9 +21,15 @@ st.header('Table of contents')
 st.markdown('''
 - [Introduction](#Introduction)
 - [Prerequisites](#Prerequisites)
-    - [Installs](#Installs)
+    - [Installation](#Installs)
     - [Imports](#Imports)
-- [Model](#Model)
+    - [Helper Functions](#Helpers)
+- [Model Architecture](#Model)
+    - [Pre-Normalization Layer](#prenorm)
+    - [Feed-Forward Network](#feedforward)
+    - [Attention Mechanism](#attention)
+    - [Transformer Network](#transformer)
+    - [Vision Transformer Model](#visiontransformer)
 - [Training](#Training)
 - [References](#References)
 - [Citations](#Citations) 
@@ -41,9 +47,34 @@ While the Transformer architecture has become the de-facto standard for natural 
 
 st.header('Prerequisites', anchor='Prerequisites')
 
-st.subheader('Installs', anchor='Installs')
+st.write("You will first need to install either DeepMind's Haiku and Google's Jax, or Facebook's PyTorch. "
+         "Deep learning greatly benefits from GPU or TPU acceleration so you will want to have access to one or many.")
+
+# Installation section
+
+st.subheader('Installation', anchor='Installs')
+
+installs_tab_1, installs_tab_2 = st.tabs(["Haiku", "PyTorch"])
+
+with installs_tab_1:
+    st.write('Install Haiku:')
+    haiku_installs = '''
+$ pip3 install -U jax jaxlib dm-haiku
+    '''
+    st.code(haiku_installs, language='bash')
+
+with installs_tab_2:
+    st.write('Install PyTorch:')
+    pytorch_installs = '''
+$ pip3 install -U torch torchvision torchaudio
+    '''
+    st.code(pytorch_installs, language='bash')
+
+# Imports section
 
 st.subheader('Imports', anchor='Imports')
+
+st.write("You will need to import the necessary libraries in your Python file or Jupyter Notebook.")
 
 imports_tab_1, imports_tab_2 = st.tabs(["Haiku", "PyTorch"])
 
@@ -72,9 +103,352 @@ from einops.layers.torch import Rearrange
     '''
     st.code(pytorch_imports, language)
 
+# Helper functions section
+
+st.subheader('Helper Functions', anchor='Helpers')
+
+st.write('Define some basic helper functions for the Model.')
+
+tab_1, tab_2 = st.tabs(["Haiku", "PyTorch"])
+
+with tab_1:
+    haiku = '''
+LayerNorm = partial(hk.LayerNorm, create_scale=True, create_offset=False, axis=-1)
+
+def pair(t):
+    return t if isinstance(t, tuple) else (t, t)
+    '''
+    st.code(haiku, language)
+
+    st.write('Haiku does not possess an Identity Layer class so we will want to define one as well.')
+
+    identity_layer_class = '''
+class IdentityLayer(hk.Module):
+    def __call__(self, x):
+        x = hk.Sequential([])
+        return x
+    '''
+    st.code(identity_layer_class, language)
+
+with tab_2:
+    pytorch = '''
+def pair(t):
+    return t if isinstance(t, tuple) else (t, t)
+    '''
+    st.code(pytorch, language)
+
 # Section for Model
 
-st.header('Model', anchor='Model')
+st.header('Model Architecture', anchor='Model')
+
+st.subheader('Pre-Normalization Layer', anchor='prenorm')
+
+tab_1, tab_2 = st.tabs(["Haiku", "PyTorch"])
+
+with tab_1:
+    haiku = '''
+class PreNorm(hk.Module):
+    def __init__(self, fn):
+        super(PreNorm, self).__init__()
+        self.norm = LayerNorm()
+        self.fn = fn
+    def __call__(self, x, **kwargs):
+        return self.fn(self.norm(x), **kwargs)
+    '''
+    st.code(haiku, language)
+
+with tab_2:
+    pytorch = '''
+class PreNorm(nn.Module):
+    def __init__(self, dim, fn):
+        super().__init__()
+        self.norm = nn.LayerNorm(dim)
+        self.fn = fn
+    def forward(self, x, **kwargs):
+        return self.fn(self.norm(x), **kwargs)
+    '''
+    st.code(pytorch, language)
+
+st.subheader('Feed-Forward Network', anchor='feedforward')
+
+st.latex(r'''
+GELU(x) = 0.5 * x * (1 + \tanh(\sqrt{\frac 2 \pi} * (x + 0.044715 * x^3)))
+''')
+
+tab_1, tab_2 = st.tabs(["Haiku", "PyTorch"])
+
+with tab_1:
+    haiku = '''
+class FeedForward(hk.Module):
+    def __init__(self, dim, hidden_dim):
+        super(FeedForward, self).__init__()
+        self.linear1 = hk.Linear(hidden_dim)
+        self.linear2 = hk.Linear(dim)
+    def __call__(self, x):
+        x = self.linear1(x)
+        x = jax.nn.gelu(x)
+        x = hk.dropout(hk.next_rng_key(), rate = 0.0, x = x)
+        x = self.linear2(x)
+        x = hk.dropout(hk.next_rng_key(), rate = 0.0, x = x)
+        return x
+    '''
+    st.code(haiku, language)
+
+with tab_2:
+    pytorch = '''
+class FeedForward(nn.Module):
+    def __init__(self, dim, hidden_dim, dropout = 0.):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(dim, hidden_dim),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(hidden_dim, dim),
+            nn.Dropout(dropout)
+        )
+    def forward(self, x):
+        return self.net(x)
+    '''
+    st.code(pytorch, language)
+
+st.subheader('Attention Mechanism', anchor='attention')
+
+tab_1, tab_2 = st.tabs(["Haiku", "PyTorch"])
+
+with tab_1:
+    haiku = '''
+class Attention(hk.Module):
+    def __init__(self, dim, heads = 8, dim_head = 64):
+        super(Attention, self).__init__()
+        inner_dim = dim_head * heads
+        project_out = not (heads == 1 and dim_head == dim)
+
+        self.heads = heads
+        self.scale = dim_head ** -0.5
+        
+        self.to_qkv = hk.Linear(output_size = inner_dim * 3, with_bias = False)
+        self.to_out = hk.Linear(dim) if project_out else IdentityLayer()
+
+    def __call__(self, x):
+        qkv = self.to_qkv(x)
+        qkv = jnp.split(qkv, 3, axis = -1)
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h=self.heads), qkv)
+
+        dots = jnp.einsum('b h i d, b h j d -> b h i j', q, k) * self.scale
+
+        attn = nn.softmax(dots, axis = -1)
+
+        out = jnp.einsum('b h i j, b h j d -> b h i d', attn, v)
+
+        out = rearrange(out, 'b h n d -> b n (h d)')
+
+        out = self.to_out(out)
+
+        out = hk.dropout(hk.next_rng_key(), rate = 0.0, x = out)
+        return out
+    '''
+    st.code(haiku, language)
+
+with tab_2:
+    pytorch = '''
+class Attention(nn.Module):
+    def __init__(self, dim, heads = 8, dim_head = 64, dropout = 0.):
+        super().__init__()
+        inner_dim = dim_head *  heads
+        project_out = not (heads == 1 and dim_head == dim)
+
+        self.heads = heads
+        self.scale = dim_head ** -0.5
+
+        self.attend = nn.Softmax(dim = -1)
+        self.dropout = nn.Dropout(dropout)
+
+        self.to_qkv = nn.Linear(dim, inner_dim * 3, bias = False)
+
+        self.to_out = nn.Sequential(
+            nn.Linear(inner_dim, dim),
+            nn.Dropout(dropout)
+        ) if project_out else nn.Identity()
+
+    def forward(self, x):
+        qkv = self.to_qkv(x).chunk(3, dim = -1)
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = self.heads), qkv)
+
+        dots = torch.matmul(q, k.transpose(-1, -2)) * self.scale
+
+        attn = self.attend(dots)
+        attn = self.dropout(attn)
+
+        out = torch.matmul(attn, v)
+        out = rearrange(out, 'b h n d -> b n (h d)')
+        return self.to_out(out)
+    '''
+    st.code(pytorch, language)
+
+st.subheader('Transformer Network', anchor='transformer')
+
+tab_1, tab_2 = st.tabs(["Haiku", "PyTorch"])
+
+with tab_1:
+    haiku = '''
+class Transformer(hk.Module):
+    def __init__(self, dim, depth, heads, dim_head, mlp_dim):
+        super(Transformer, self).__init__()
+        self.layers = []
+        for _ in range(depth):
+            self.layers.append([
+                PreNorm(Attention(dim, heads=heads, dim_head=dim_head)),
+                PreNorm(FeedForward(dim, mlp_dim))
+            ])
+    def __call__(self, x):
+        for attn, ff in self.layers:
+            x = attn(x) + x
+            x = ff(x) + x
+        return x
+    '''
+    st.code(haiku, language)
+
+with tab_2:
+    pytorch = '''
+class Transformer(nn.Module):
+    def __init__(self, dim, depth, heads, dim_head, mlp_dim, dropout = 0.):
+        super().__init__()
+        self.layers = nn.ModuleList([])
+        for _ in range(depth):
+            self.layers.append(nn.ModuleList([
+                PreNorm(dim, Attention(dim, heads = heads, dim_head = dim_head, dropout = dropout)),
+                PreNorm(dim, FeedForward(dim, mlp_dim, dropout = dropout))
+            ]))
+    def forward(self, x):
+        for attn, ff in self.layers:
+            x = attn(x) + x
+            x = ff(x) + x
+        return x
+    '''
+    st.code(pytorch, language)
+
+st.subheader('Vision Transformer Model', anchor='visiontransformer')
+
+tab_1, tab_2 = st.tabs(["Haiku", "PyTorch"])
+
+with tab_1:
+    haiku = '''
+class VitBase(hk.Module):
+    def __init__(self, *, image_size, patch_size, num_classes, dim, depth, heads, mlp_dim, pool = 'cls', channels = 3, dim_head = 64):
+        super(VitBase, self).__init__()
+
+        image_height, image_width = pair(image_size)
+        patch_height, patch_width = pair(patch_size)
+
+        self.patch_height = patch_height
+        self.patch_width = patch_width
+
+        assert image_height % patch_height == 0 and image_width % patch_width == 0
+
+        num_patches = (image_height // patch_height) * (image_width // patch_width)
+        assert pool in {'cls', 'mean'}, 'pool type must be either cls (cls token) or mean (mean pooling)'
+
+        self.to_patch_embedding = hk.Linear(dim)
+
+        self.pos_embedding = hk.get_parameter('pos_embedding', shape = [1, num_patches + 1, dim], init = jnp.zeros)
+        self.cls_token = hk.get_parameter('cls_token', shape = [1, 1, dim], init = jnp.zeros)
+
+        self.transformer = Transformer(dim, depth, heads, dim_head, mlp_dim)
+
+        self.pool = pool
+
+        self.mlp_head = hk.Sequential([
+            LayerNorm(),
+            hk.Linear(num_classes)
+        ])
+
+    def __call__(self, img):
+
+        img = rearrange(img, 'b (h p1) (w p2) c -> b (h w) (p1 p2 c)', p1 = self.patch_height, p2 = self.patch_width)
+
+        x = self.to_patch_embedding(img)
+        b, n, _ = x.shape
+
+        cls_tokens = repeat(self.cls_token, '() n d -> b n d', b=b)
+
+        x = jnp.concatenate([cls_tokens, x], axis = 1)
+        x += self.pos_embedding[:, :(n + 1)]
+        x = hk.dropout(hk.next_rng_key(), rate = 0.0, x = x)
+
+        x = self.transformer(x)
+
+        if self.pool == 'mean':
+            x = jnp.mean(x, axis = 1)
+        else:
+            x = x[:, 0]
+
+        x = self.mlp_head(x)
+
+        return x
+    '''
+    st.code(haiku, language)
+
+    st.write('Haiku requires ')
+
+    haiku_transform = '''
+def ViT(**kwargs):
+    @hk.transform
+    def inner(img):
+        return VitBase(**kwargs)(img)
+    return inner
+    '''
+    st.code(haiku_transform, language)
+
+with tab_2:
+    pytorch = '''
+class ViT(nn.Module):
+    def __init__(self, *, image_size, patch_size, num_classes, dim, depth, heads, mlp_dim, pool = 'cls', channels = 3, dim_head = 64, dropout = 0., emb_dropout = 0.):
+        super().__init__()
+        image_height, image_width = pair(image_size)
+        patch_height, patch_width = pair(patch_size)
+
+        assert image_height % patch_height == 0 and image_width % patch_width == 0, 'Image dimensions must be divisible by the patch size.'
+
+        num_patches = (image_height // patch_height) * (image_width // patch_width)
+        patch_dim = channels * patch_height * patch_width
+        assert pool in {'cls', 'mean'}, 'pool type must be either cls (cls token) or mean (mean pooling)'
+
+        self.to_patch_embedding = nn.Sequential(
+            Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1 = patch_height, p2 = patch_width),
+            nn.Linear(patch_dim, dim),
+        )
+
+        self.pos_embedding = nn.Parameter(torch.randn(1, num_patches + 1, dim))
+        self.cls_token = nn.Parameter(torch.randn(1, 1, dim))
+        self.dropout = nn.Dropout(emb_dropout)
+
+        self.transformer = Transformer(dim, depth, heads, dim_head, mlp_dim, dropout)
+
+        self.pool = pool
+        self.to_latent = nn.Identity()
+
+        self.mlp_head = nn.Sequential(
+            nn.LayerNorm(dim),
+            nn.Linear(dim, num_classes)
+        )
+
+    def forward(self, img):
+        x = self.to_patch_embedding(img)
+        b, n, _ = x.shape
+
+        cls_tokens = repeat(self.cls_token, '1 1 d -> b 1 d', b = b)
+        x = torch.cat((cls_tokens, x), dim=1)
+        x += self.pos_embedding[:, :(n + 1)]
+        x = self.dropout(x)
+
+        x = self.transformer(x)
+
+        x = x.mean(dim = 1) if self.pool == 'mean' else x[:, 0]
+
+        x = self.to_latent(x)
+        return self.mlp_head(x)
+    '''
+    st.code(pytorch, language)
 
 # Section for Training
 
@@ -87,3 +461,18 @@ st.header('References', anchor='References')
 # Section for Citations
 
 st.header('Citations', anchor='Citations')
+
+
+tab_1, tab_2 = st.tabs(["Haiku", "PyTorch"])
+
+with tab_1:
+    haiku = '''
+
+    '''
+    st.code(haiku, language)
+
+with tab_2:
+    pytorch = '''
+
+    '''
+    st.code(pytorch, language)
